6cfd62464bfffcf265627603abff84f527e7bdb6
Emilio Islas <exinyo@gmail.com>
2017-05-06 22:14:28 -0700

    Css watch
    Watches for css changes for hotreloading
    Also watches the theme value for which css to use

---
+++ app/js/css.js
@@ -0,0 +1,36 @@
+import fs from 'fs'
+import path from 'path'
+import { observe } from 'mobx'
+import { ipcRenderer } from 'electron'
+
+import Store from './manager/store'
+
+document.getElementById('cssLight').disabled = true;
+document.getElementById('cssDark').disabled  = true;
+
+if (process.env.PRODUCTION === 'false') {
+  fs.watch(path.join(__dirname, './../css'), (eventType, filename) => {
+    if (eventType === 'change') {
+      const links = document.getElementsByTagName('link');
+      for (let i = 0; i < links.length; i++) {
+        let link = links[i];
+        if (link.rel === 'stylesheet') {
+          const time = `?${Date.now()}`
+          link.href = link.href.replace(/\?.*$/, time);
+        }
+      }
+    }
+  })
+}
+
+observe(Store, 'theme', ({newValue}) => {
+  document.getElementById('cssLight').disabled = true;
+  document.getElementById('cssDark').disabled = true;
+  if (newValue === 'light') {
+    document.getElementById('cssLight').disabled = false;
+  } else {
+    document.getElementById('cssDark').disabled = false;
+  }
+})
+
+Store.theme = ipcRenderer.sendSync('getTheme');
