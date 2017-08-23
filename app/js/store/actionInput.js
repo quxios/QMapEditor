dd2646705cf6c1b0d6071fd44fd0a1e6ed7bec34
Emilio Islas <exinyo@gmail.com>
2017-08-22 18:00:07 -0700

    Input Manager

--- app/js/store/actionInput.js
+++ app/js/store/actionInput.js
@@ -2,20 +2,59 @@ import { action } from 'mobx'
 
 export default (C) => {
   return class ActionInput extends C {
+    _keyState = {};
+    _lastDown = null;
+    _lastReleased = null;
+
+    constructor() {
+      super();
+      document.addEventListener('keydown', ::this._onKeydown);
+      document.addEventListener('keyup', ::this._onKeyup);
+    }
+
+    @action.bound
+    isTriggered(keyCode) {
+      return this._lastDown === keyCode;
+    }
+
     @action.bound
     isPressed(keyCode) {
-      return this.keyState[keyCode];
+      return this._keyState[keyCode];
+    }
+
+    @action.bound
+    isLongPressed(keyCode) {
+      if (!this.isPressed(keyCode)) return false;
+      const dt = Date.now() - this._keyState[keyCode];
+      return this.isTriggered(keyCode) || dt > 300;
+    }
+
+    @action.bound
+    isReleased(keyCode) {
+      return this._lastReleased === keyCode;
     }
 
     @action.bound
     _onKeydown(event) {
       const { keyCode } = event;
-      this.keyState[keyCode] = true;
+      const oldState = this._keyState[keyCode];
+      if (!oldState) {
+        this._keyState[keyCode] = Date.now();
+        this._lastDown = keyCode;
+      }
     }
 
     @action.bound
     _onKeyup(event) {
-      this.keyState[event.keyCode] = false;
+      const { keyCode } = event;
+      this._keyState[keyCode] = false;
+      this._lastReleased = keyCode;
+    }
+
+    @action.bound
+    updateInput() {
+      this._lastDown = null;
+      this._lastReleased = null;
     }
   }
 }
